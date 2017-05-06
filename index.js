f44ea936ce284f9c98777e71a5834d1fa8ffe8c3
rafael zegarra <rafael.zegarra@ucsp.edu.pe>
2017-05-05 21:47:06 -0500

    archivos estaticos

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static('public'));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.listen(3000, function () {
